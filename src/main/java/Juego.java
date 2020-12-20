e51df71349b2486677d39ac6e9d157964f6b1f1c
Arturo <a.avendano04@ufromail.cl>
2020-12-20 20:06:27 -0300

    Inicio de proyecto, modificacion de clase juego, inclusion de void pelear() y morir()

--- src/main/java/Juego.java
+++ src/main/java/Juego.java
@@ -3,4 +3,27 @@ public class Juego {
     public static void lanzarJuego(){
 
     }
+    void pelear(int[] jugador){
+        int[] enemigo= new int[2];
+        enemigo[0]= (int) Math.floor(Math.random()*70+30);
+        enemigo[1]= (int) Math.floor(Math.random()*7+3);
+        boolean turno=true;
+        while(jugador[0]>0 && enemigo[0]>0){
+            if(turno){
+                enemigo[0]=enemigo[0]-jugador[1];
+                turno=!turno;
+            }
+            else{
+                jugador[0]=jugador[0]-enemigo[1];
+                turno=!turno;
+            }
+        }
+        if(jugador[0]<0){
+            morir();
+        }
+    }
+    void morir(){
+        System.err.println("Te moriste, fin del juego.");
+        System.exit(1);
+    }
 }
