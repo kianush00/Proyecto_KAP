21d8cc6e8198e00a132fe95628f2751ab22906d1
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-12-22 20:29:44 -0300

    -Se modifica el método abrirTienda: ahora contiene un bucle while que permite elegir una de las 3 opciones, y luego se opera bajo esa opción por medio de un switch case. -se agregan los métodos opcionUnoTienda y opcionDosTienda, los cuales operan bajo la opción elegida. -se agrega el método elegirOpcionYValidar, el cual engloba los métodos de validarOpcionEntera,ingresarEntero y opcionEstaDentroDelRango

--- src/main/java/Juego.java
+++ src/main/java/Juego.java
@@ -15,9 +15,9 @@ public class Juego {
 
         for(int i=1; i <= 30; i++){
             //Se desarrolla el juego
-
+            //...
+            intentarAbrirTienda(estadisticasJugador,inventarioJugador);
         }
-
     }
 
     public static void intentarAbrirTienda(int[] estadisticasJugador, int[] inventarioJugador){
@@ -27,10 +27,34 @@ public class Juego {
     }
 
     public static void abrirTienda(int[] estadisticasJugador, int[] inventarioJugador){
-        String[] tipoTienda = {"hospital","cuartel"};    //La tienda puede ser un hospital o un cuartel
+        String[] tipoTienda = {"hospital","cuartel"};       //La tienda puede ser un hospital o un cuartel
+        boolean salirTienda = false;        //bandera que indica si el jugador desea salir de la tienda
+        int opcion;        //la opción que se elige dentro de las que correspodnan
         String tiendaActual = generarTiendaActual(tipoTienda);   //Se genera la tienda de forma aleatoria
 
         darBienvenidaTienda(tiendaActual);
+        while(!salirTienda){    //se ejecuta bucle mientras el jugador no desee salir de la tienda
+            mostrarOpcionesTienda();    //1.-Comprar  2.-Vender  3.-Salir
+            switch (elegirOpcionYValidar(1,3)){
+                case 1:
+                    opcionUnoTienda(estadisticasJugador,inventarioJugador);
+                break;
+                case 2:
+                    opcionDosTienda(estadisticasJugador,inventarioJugador);
+                break;
+                case 3:
+                    salirTienda = true;
+                break;
+            }
+        }
+    }
+
+    public static void opcionUnoTienda(int[] estadisticasJugador, int[] inventarioJugador){
+
+    }
+
+    public static void opcionDosTienda(int[] estadisticasJugador, int[] inventarioJugador){
+
     }
 
     public static String generarTiendaActual(String[] tipoTienda){
@@ -39,13 +63,50 @@ public class Juego {
 
     public static void darBienvenidaTienda(String tiendaActual){
         println("\nBienvenido al " + tiendaActual + ". ¿Qué desea?");
+    }
+
+    public static void mostrarOpcionesTienda(){
+        println("Elige una opción: ");
         println("1. Comprar");
         println("2. Vender");
         println("3. Salir");
     }
 
+    public static int elegirOpcionYValidar(int minimaOpcion, int maximaOpcion){
+        int opcion = -1;    //se inicializa la opción
+        boolean opcionEsValida = false;
+
+        while(!opcionEsValida){
+            validarOpcionEntera();
+            opcion = ingresarEntero();
+            opcionEsValida = opcionEstaDentroDelRango(opcion,minimaOpcion,maximaOpcion);
+        }
+
+        return opcion;
+    }
+
+    public static void validarOpcionEntera(){
+        while(!input.hasNextInt()){
+            System.err.println("La opción ingresada no es un entero. Intenta nuevamente:");
+            input.next();   //pasa al siguiente iterador
+        }
+    }
+
+    public static boolean opcionEstaDentroDelRango(int opcion, int minimaOpcion, int maximaOpcion){
+        if((opcion < minimaOpcion) || (opcion > maximaOpcion)){
+            System.err.println("La opción ingresada no se encuentra dentro del rango. Intenta nuevamente:");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     public static void mostrarFichasActuales(int[] inventarioJugador){
         println("\nFichas actuales: " + inventarioJugador[3]);
+    }
+
+    public static int ingresarEntero(){
+        return input.nextInt();
     }
 
     public static void println(String mensaje){
