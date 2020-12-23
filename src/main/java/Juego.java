6bc419bba9957950b1a8406156ca550cf39ffc90
Arturo <a.avendano04@ufromail.cl>
2020-12-22 21:11:19 -0300

    Inclusión del método curar y modificación del método pelea()

--- src/main/java/Juego.java
+++ src/main/java/Juego.java
@@ -5,35 +5,45 @@ public class Juego {
     public static void lanzarJuego(){
 
     }
-    void pelear(int[] jugador){
+    void pelear(int[] estadisticasJugador , int[] inventarioJugador){
         int[] enemigo= new int[2];
         Scanner entrada = new Scanner(System.in);
         enemigo[0]= (int) Math.floor(Math.random()*70+30);
         enemigo[1]= (int) Math.floor(Math.random()*7+3);
         boolean turno=true;
-        while(jugador[0]>0 && enemigo[0]>0){
-            System.out.println("Inicia la batalla del nivel " + jugador[3]);
+        while(estadisticasJugador [0]>0 && enemigo[0]>0){
+            System.out.println("Inicia la batalla del nivel " + estadisticasJugador [3]);
             if(turno){
-                System.out.print("Opcion 1 para atacar, 2 para pasar, 3 para huir: ");
+                System.out.print("Opcion 1 para atacar, 2 para pasar, 3 para huir, finalmente 4 para curarte: ");
                 while (!entrada.hasNextInt()) {
                     System.err.println("Debe ser un numero");
                     System.out.print("Intentalo de nuevo: ");
                     entrada.next();
                 }
                 int opcion = entrada.nextInt();
-                if(opcion==1) {
-                    enemigo[0] = enemigo[0] - jugador[1];
-                    System.out.println("Has atacado al enemigo con " + jugador[1] + "Puntos de daño, su vida actual es" + enemigo[0]);
-                }
-                if(opcion==2){
-                    System.out.println("Has pasado");
-                }
-                if(opcion==3) {
-                    int escapar=(int) Math.floor(Math.random()*2+1);
-                    if(escapar==1){
+                switch(opcion) {
+                    case 1:
+                        if(inventarioJugador[0]>0) {
+                            enemigo[0] = enemigo[0] - estadisticasJugador[1];
+                            inventarioJugador[0] = inventarioJugador[0] - estadisticasJugador[4];
+                            System.out.println("Has atacado al enemigo con " + estadisticasJugador[1] + "Puntos de daño, su vida actual es" + enemigo[0]);
+                        }
+                        else {
+                            System.err.println("no te quedan balas, no puedes atacar");
+                        }
                         break;
-                    }
-
+                    case 2:
+                        System.out.println("Has pasado");
+                        break;
+                    case 3:
+                        int escapar=(int) Math.floor(Math.random()*2+1);
+                        if(escapar==1){
+                            break;
+                        }
+                        break;
+                    case 4:
+                        curar(estadisticasJugador [0]);
+                        break;
                 }
                 turno = !turno;
             }
@@ -44,12 +54,12 @@ public class Juego {
                     System.out.println("El enemigo ha fallado.");
                 }
                 else
-                jugador[0]=jugador[0]-enemigo[1];
-                System.out.println("Te han atacado con "+enemigo[1] + "Puntos de daño, tu vida actual es"+ jugador[0]);
+                estadisticasJugador [0]=estadisticasJugador [0]-enemigo[1];
+                System.out.println("Te han atacado con "+enemigo[1] + "Puntos de daño, tu vida actual es"+ estadisticasJugador [0]);
                 turno=!turno;
             }
         }
-        if(jugador[0]<0){
+        if(estadisticasJugador [0]<0){
             morir();
         }
     }
@@ -61,5 +71,9 @@ public class Juego {
         System.out.println("Has llegado al final de la aventura, felicidades.");
         System.out.println("Ya puedes escapar.");
         System.exit(1);
+    }
+    void curar(int vida){
+        vida=vida+50;
+        System.out.println("Ahora tu vida actual es " + vida);
     }
 }
